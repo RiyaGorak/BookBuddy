9e1d0cacd1b270230317776c1a57091343764234
RiyaGorak <gorakriya@gmail.com>
2026-06-25 15:15:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click",()=>{
+
+    alert("Welcome to BookBuddy 🚀");
+
+    // Later:
+    // window.location.href = "home.html";
+
+});
